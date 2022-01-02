1b24c857191c22e2348353a46f86b63f0aa4c2d5
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2022-01-02 22:02:25 +0100

    feat: Add (very basic) navigationh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
+import Navigation from "./components/Navigation";
 import Search from "./components/Search";
 import Translate from "./components/Translate";
 
@@ -37,19 +38,52 @@ const dropdownOptions = [
 const App = () => {
   const [selection, setSelection] = useState(dropdownOptions[0]);
 
+  const showAccordion = () => {
+    if (window.location.pathname === "/") {
+      return <Accordion items={items} />;
+    }
+  };
+
+  const showSearch = () => {
+    if (window.location.pathname === "/search") {
+      return <Search />;
+    }
+  };
+
+  const showDropdown = () => {
+    if (window.location.pathname === "/dropdown") {
+      return (
+        <div className="dropdown">
+          <Dropdown
+            label="Please select a color"
+            selected={selection}
+            onSelectedChange={setSelection}
+            options={dropdownOptions}
+          />
+          <h2 style={{ color: selection.value }}>
+            This text is {selection.label}
+          </h2>
+        </div>
+      );
+    }
+  };
+
+  const showTranslate = () => {
+    if (window.location.pathname === "/translate") {
+      return <Translate />;
+    }
+  };
+
+  console.log(window.location.pathname);
   return (
     <div>
       <h1>Widgets App</h1>
-      <Dropdown
-        label="Please select a color"
-        selected={selection}
-        onSelectedChange={setSelection}
-        options={dropdownOptions}
-      />
-      <h2 style={{ color: selection.value }}>This text is {selection.label}</h2>
+      <Navigation />
 
-      <hr />
-      <Translate />
+      {showAccordion()}
+      {showSearch()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+const Navigation = () => {
+        
+    return (
+      <div className="ui tabular menu">
+        <a href="/" className="item">
+          Accordion
+        </a>
+        <a href="/search" className="item">
+          Search
+        </a>
+        <a href="/dropdown" className="item">
+          Dropdown
+        </a>
+        <a href="/translate" className="item">
+          Translate
+        </a>
+      </div>
+    );
+};
+
+export default Navigation;
